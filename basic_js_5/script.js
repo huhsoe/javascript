5318af2098f64de3eb6b94d603da752aa08f11ed
huhsoe <kh01sophie@gmail.com>
2025-10-13 18:30:17 +0300

    i hate maths

---
+++ basic_js_5/script.js
@@ -0,0 +1,8 @@
+let a = 2 * 2 + 2 ;
+console.log(a);
+
+const x = Math.round((Math.sin(54) * Math.cos(16)) ** 2);
+console.log(x);
+
+const y = Math.round(((16 * Math.sqrt(13.2 * 71.90)) / (2.4 / (7 ** 4)) + 3 * (Math.sqrt(49))) * (2 ** 7));
+console.log(y);
