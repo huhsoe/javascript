d15607d70155c2ce9bb0ab9f7c76d18bc9e1a3f0
huhsoe <kh01sophie@gmail.com>
2025-10-14 18:05:55 +0300

    next

--- basic_js_5/script.js
+++ basic_js_5/script.js
@@ -6,3 +6,18 @@ console.log(x);
 
 const y = Math.round(((16 * Math.sqrt(13.2 * 71.90)) / (2.4 / (7 ** 4)) + 3 * (Math.sqrt(49))) * (2 ** 7));
 console.log(y);
+
+
+const b = 3;
+const c = 4;
+const d = 5;
+const e = 6;
+
+function isEven (number) {
+    return number % 2 === 0 // Если число делится на 2 без остатка (остаток 0), то оно четное (true)
+};
+
+console.log(isEven(b));
+console.log(isEven(c));
+console.log(isEven(d));
+console.log(isEven(e));
