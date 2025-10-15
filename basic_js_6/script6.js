b276761d25845bdb293d29503bd54d6375c98526
huhsoe <kh01sophie@gmail.com>
2025-10-15 17:32:50 +0300

    second task

--- basic_js_6/script6.js
+++ basic_js_6/script6.js
@@ -9,3 +9,18 @@ if (num > 0) {
 };
 
 
+let height = prompt ('Your height (m):');
+let weight = prompt ('Your weight (kg):');
+let BMI = Math.round(weight / (height**2));
+
+console.log (BMI);
+
+if (BMI >= 30) {
+    document.body.innerHTML = 'Obesity :(';
+} else if (BMI >= 25) {
+    document.body.innerHTML = 'Overweight';
+} else if (BMI >= 18) {
+    document.body.innerHTML = 'Normal';
+} else {
+    document.body.innerHTML = 'Underweight';
+};
