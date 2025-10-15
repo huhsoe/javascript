132877d8545d0ab776b6835a9a86807e6913b8ec
huhsoe <kh01sophie@gmail.com>
2025-10-15 18:06:17 +0300

    final tasks

--- basic_js_6/script6.js
+++ basic_js_6/script6.js
@@ -24,3 +24,84 @@ if (BMI >= 30) {
 } else {
     document.body.innerHTML = 'Underweight';
 };
+
+
+
+const month = 3;
+
+let monthName;
+
+switch (month) {
+    case 1:
+        monthName = 'Январь';
+        break;
+
+    case 2:
+        monthName = 'Февраль';
+        break;
+
+    case 3:
+        monthName = 'Март';
+        break;
+
+    case 4:
+        monthName = 'Апрель';
+        break;
+
+    case 5:
+        monthName = 'Май';
+        break;
+
+    case 6:
+        monthName = 'Июнь';
+        break;
+
+    case 7:
+        monthName = 'Июль';
+        break;
+
+    case 8:
+        monthName = 'Август';
+        break;
+
+    case 9:
+        monthName = 'Сентябрь';
+        break;
+
+    case 10:
+        monthName = 'Октябрь';
+        break;
+
+    case 11:
+        monthName = 'Ноябрь';
+        break;
+
+    case 12:
+        monthName = 'Декабрь';
+        break;
+
+    default:
+        monthName = 'Неверный месяц';
+}
+
+console.log (monthName);
+
+
+const mood = 'Fine';
+let moodMessage;
+
+switch (mood) {
+    case 'Bad':
+        moodMessage = 'Sorry to hear that!';
+        break;
+    case 'Good', 'Great':
+        moodMessage = 'Great to hear that!';
+        break;
+    case 'Ok', 'Fine':
+        moodMessage = 'Hope it will get better!';
+        break;
+    default:
+        moodMessage = 'Tell me what is your mood';
+}
+
+console.log (moodMessage);
