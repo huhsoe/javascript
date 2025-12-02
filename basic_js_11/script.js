b0ae977b42bab0e2d5da23a94434e14c3b345e71
huhsoe <kh01sophie@gmail.com>
2025-12-03 00:15:16 +0300

    2 task im dead

--- basic_js_11/script.js
+++ basic_js_11/script.js
@@ -1,22 +1,5 @@
-/*const str = 'nurses run';
-const strToArr = function(str) {
-    return [...str].filter(char => char !== ' ');
-};
-
-console.log(strToArr(str));
-
-
-function checkIfPalindrome(str) {
-    const preparedStr = strToArr(str).reverse();
-
-    if ( strToArr(str) === preparedStr ) {
-        return true;
-    } else
-        return false;
-};
-
-console.log(checkIfPalindrome(str));*/
-
+const task1 = '1st task';
+console.log(task1);
 
 function checkIfPalindrome(str) {
     const preparedStr = str.toLowerCase();
@@ -28,4 +11,26 @@ function checkIfPalindrome(str) {
 };
 
 console.log(checkIfPalindrome('Nurses run'));
-console.log(checkIfPalindrome('hhjighkfghdgsdf'));
+console.log(checkIfPalindrome('Hello'));
+console.log(checkIfPalindrome('rotator'));
+
+const task2 = '2nd task';
+console.log(task2);
+
+function findShortestWord(sentence) {
+    if (!sentence || sentence.trim().length === 0) {
+        return '';
+    }
+    const words = sentence.trim().split(' ');
+
+    let shortestIndex = 0;
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length < words[shortestIndex].length) {
+            shortestIndex = i;
+        }
+    }
+    
+    return words[shortestIndex];
+};
+
+console.log(findShortestWord('This is really hard for me'));
