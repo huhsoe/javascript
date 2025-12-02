335e04d522655ebe5f19e195b34da43f4aeee44b
huhsoe <kh01sophie@gmail.com>
2025-12-02 20:54:49 +0300

    1 task is done

--- basic_js_11/script.js
+++ basic_js_11/script.js
@@ -21,10 +21,11 @@ console.log(checkIfPalindrome(str));*/
 function checkIfPalindrome(str) {
     const preparedStr = str.toLowerCase();
     const resultStr = [...preparedStr].filter(char => char !== ' ');
-     if ( resultStr === resultStr.reverse() ) {
+     if ( resultStr.join('') === resultStr.reverse().join('') ) {
         return true;
     } else
         return false;
 };
 
 console.log(checkIfPalindrome('Nurses run'));
+console.log(checkIfPalindrome('hhjighkfghdgsdf'));
