d47cad8c602e7b9e0a645db1056bce5e388b4f07
huhsoe <kh01sophie@gmail.com>
2025-12-11 00:48:03 +0300

    t3

--- basic_js_11/script.js
+++ basic_js_11/script.js
@@ -34,3 +34,22 @@ function findShortestWord(sentence) {
 };
 
 console.log(findShortestWord('This is really hard for me'));
+
+
+
+const task3 = '3rd task';
+console.log(task3);
+
+
+function formatPhoneNumber(numbers) {
+    const numStr = numbers.toString();
+    if (numStr.length < 10) {
+        return 'Wrong format';
+    }
+    const lastTenDigits = numStr.slice(-10);
+
+    return `8 (${lastTenDigits.slice(0, 3)}) ${lastTenDigits.slice(3, 6)} - ${lastTenDigits.slice(6)}`;
+}
+
+console.log(formatPhoneNumber(81234567890));
+console.log(formatPhoneNumber(812345));
